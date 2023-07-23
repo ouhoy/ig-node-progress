3cafbe780d32a585b50a4bffba47f0aff1c73876
ouhoy <kimkinon@gmail.com>
2023-07-23 22:23:58 +0100

    Access Bio and Write New Bio

--- server.js
+++ server.js
@@ -2,9 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const puppeteer_1 = require("puppeteer");
 const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
 const imgFolder = path.join(__dirname, 'img');
 const url = "https://instagram.com";
+const editProfilePage = `https://www.instagram.com/accounts/edit/`;
+// Login Credentials
 const emailInput = `#loginForm > div > div:nth-child(1) > div > label > input`;
+const passwordInput = `#loginForm > div > div:nth-child(2) > div > label > input`;
+const loginButton = `#loginForm > div > div:nth-child(3) > button`;
+//Profile Configurations
+const bioInput = `textarea`;
 (async () => {
     const browser = await puppeteer_1.default.launch({ headless: false });
     const page = await browser.newPage();
@@ -15,6 +23,20 @@ const emailInput = `#loginForm > div > div:nth-child(1) > div > label > input`;
     const currentTime = new Date();
     const creationDate = currentTime.toString().slice(0, 24);
     await page.screenshot({ path: `${imgFolder}/screenshot-${creationDate.replaceAll(":", "-")}.jpg` });
-    await browser.close();
+    await page.locator(emailInput).fill(process.env.USER_NAME);
+    await page.locator(passwordInput).fill(process.env.PASSWORD);
+    await page.locator(loginButton).click();
+    await page.waitForSelector(`iframe`, { timeout: 30000 });
+    await page.goto(editProfilePage);
+    // console.log("We there rn!")
+    await page.locator("#pepBio").click();
+    console.log("Bio is there");
+    await page.evaluateHandle(() => {
+        const bioText = document.getElementById("pepBio");
+        bioText.value = `${bioText.value} \n ${new Date()}`;
+        return;
+    });
+    await page.type('textarea[id]', '', { delay: 20 });
+    // await browser.close();
 })();
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,13 +1,23 @@
 import puppeteer from 'puppeteer';
 import * as path from "path";
 
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+
 
 const imgFolder = path.join(__dirname, 'img');
-
-
 const url = "https://instagram.com";
+const editProfilePage = `https://www.instagram.com/accounts/edit/`;
 
-const emailInput = `#loginForm > div > div:nth-child(1) > div > label > input`;
+// Login Credentials
+const emailInput: string = `#loginForm > div > div:nth-child(1) > div > label > input`
+const passwordInput: string = `#loginForm > div > div:nth-child(2) > div > label > input`;
+const loginButton: string = `#loginForm > div > div:nth-child(3) > button`;
+
+//Profile Configurations
+
+const bioInput = `textarea`;
 
 (async () => {
     const browser = await puppeteer.launch({headless: false});
@@ -23,5 +33,33 @@ const emailInput = `#loginForm > div > div:nth-child(1) > div > label > input`;
     const creationDate = currentTime.toString().slice(0, 24)
     await page.screenshot({path: `${imgFolder}/screenshot-${creationDate.replaceAll(":", "-")}.jpg`})
 
-    await browser.close();
+
+    await page.locator(emailInput).fill(process.env.USER_NAME);
+    await page.locator(passwordInput).fill(process.env.PASSWORD);
+
+
+    await page.locator(loginButton).click();
+
+
+    await page.waitForSelector(`iframe`, {timeout: 30_000});
+
+    await page.goto(editProfilePage)
+
+    // console.log("We there rn!")
+
+    await page.locator("#pepBio").click();
+
+    console.log("Bio is there")
+
+    await page.evaluateHandle(() => {
+        const bioText = (<HTMLInputElement>document.getElementById("pepBio"));
+        bioText.value = `${bioText.value} \n ${new Date()}`;
+
+        return
+    });
+
+    await page.type('textarea[id]', '', {delay: 20})
+
+
+    // await browser.close();
 })();
