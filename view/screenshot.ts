a393668cd6197d7b5d2804bbc451f74079cdbf18
ouhoy <kimkinon@gmail.com>
2023-07-30 14:29:49 +0100

    Fix Mkdir folder method on screenshot file
    -Could not take a screenshot when the folder does not exist -FIXED-
    -Could not create a child folder when parent folder does not exist  -FIXED-
    
    --Creation Date should be validated in case two screenshots were taken at the same specific time with the same seconds -PENDING-

--- view/screenshot.ts
+++ view/screenshot.ts
@@ -3,14 +3,19 @@ import * as fs from "fs"
 import {Page} from "puppeteer";
 
 
-const takeScreenshot = async (folder: string, page: Page) => {
-    const folderPath: string = path.join(__dirname, folder);
+const takeScreenshot = async (page: Page, ...folder: string[]) => {
+    const folderPath: string = path.join(__dirname, ...folder);
+
+    const currentTime = new Date();
+    const creationDate = currentTime.toString().slice(0, 24)
 
     if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath);
+
+        fs.mkdirSync(folderPath, {recursive: true});
+
     }
 
-    await page.screenshot({path: folderPath})
+    await page.screenshot({path: `${folderPath}/${creationDate.replaceAll(":", "-")}.jpg`})
 }
 
 
